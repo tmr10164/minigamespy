0e06f3afbb3a18457eec4ed3f5fd1487d493da7f
DoomGod <70023601+elezor-dev@users.noreply.github.com>
2022-08-19 17:08:11 +0000

    Optimizated

--- RockPaperScisors.py
+++ RockPaperScisors.py
@@ -1,6 +1,6 @@
 import random
 rps = ["🗻","📄","✂"]
-while 1 == 1 :
+while 1:
     player = int(input("напиши 1 если камень, 2 если бумага , 3 если ножницы")) - 1
     if player > 2 or player < 0 :
         break
@@ -8,7 +8,7 @@ while 1 == 1 :
     print(rps[bot],"X",rps[player])
     if player == bot :
         print("ничья")
-    elif player - bot == 1 or (player == 0 and bot == 2) :
+    elif player == 0 and bot == 2 :
         print("Ты победил!")
-    elif player - bot == -1 or (player == 2 and bot == 0) :
+    elif player == 2 and bot == 0:
         print("ты проиграл")
